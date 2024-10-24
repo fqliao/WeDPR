b773f3eee1a7f17200d663f9c8189a916b04745e
caryliao(廖飞强) <caryliao@webank.com>
2024-10-24 15:59:28 +0800

    fix check param format

--- wedpr-components/user/src/main/java/com/webank/wedpr/components/user/requests/UserRegisterRequest.java
+++ wedpr-components/user/src/main/java/com/webank/wedpr/components/user/requests/UserRegisterRequest.java
@@ -1,24 +1,22 @@
 package com.webank.wedpr.components.user.requests;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
 import lombok.Data;
 
 /** Created by caryliao on 2024/7/18 16:58 */
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class UserRegisterRequest {
-    @Pattern(regexp = "^[a-zA-Z0-9_-]{3,18}$", message = "username format error")
+    @NotBlank(message = "username is not be empty")
     private String username;
 
     @NotBlank(message = "password is not be empty")
     private String password;
 
-    @Pattern(regexp = "^[1]([3-9])[0-9]{9}$", message = "phone format error")
+    @NotBlank(message = "phone is not be empty")
     private String phone;
 
-    @Email(message = "email format error")
+    @NotBlank(message = "email is not be empty")
     private String email;
 }
